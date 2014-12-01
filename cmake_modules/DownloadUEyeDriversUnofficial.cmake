52d82193604d82093fe81206771e620059c2ded7
Anqi Xu <anqixu@cim.mcgill.ca>
2014-12-01 17:00:01 -0500

    updated warning message

--- cmake_modules/DownloadUEyeDriversUnofficial.cmake
+++ cmake_modules/DownloadUEyeDriversUnofficial.cmake
@@ -2,22 +2,7 @@
 # https://bitbucket.org/kmhallen/ueye/src/4d8e78311e9d1ba4db3327b89862c3fa3ae602d1/CMakeLists.txt?at=default
 
 function(download_ueye_drivers UEYE_LIBRARY_VAR UEYE_INCLUDE_DIR_VAR)
-  message(WARNING "!!!!!!!!!!!!!!!!!!!!")
-  message(WARNING "!!!!!!!!!!!!!!!!!!!!")
-  message(WARNING "!!!!! WARNING  !!!!!")
-  message(WARNING "!!!!!!!!!!!!!!!!!!!!")
-  message(WARNING "!!!!!!!!!!!!!!!!!!!!")
-  message(WARNING " ")
-  message(WARNING "The official IDS uEye drivers were not detected on your machine. A temporary version of the header/library will be downloaded, to ensure that this package builds.")
-  message(WARNING " ")
-  message(WARNING "Nevertheless, you (or a system administrator) MUST still download and install the official IDS uEye drivers. Also make sure that the IDS daemon (ueyeusbdrc) is running.")
-  message(WARNING "(http://en.ids-imaging.com/download-ueye.html)")
-  message(WARNING " ")
-  message(WARNING "!!!!!!!!!!!!!!!!!!!!")
-  message(WARNING "!!!!!!!!!!!!!!!!!!!!")
-  message(WARNING "!!!!! WARNING  !!!!!")
-  message(WARNING "!!!!!!!!!!!!!!!!!!!!")
-  message(WARNING "!!!!!!!!!!!!!!!!!!!!")
+  message(WARNING "The official IDS uEye drivers were not detected on your machine. A temporary version of the header/library will be downloaded locally to your ROS buildspace, to ensure that this package compiles. Nevertheless, you (or a system administrator) MUST still download and install the official IDS uEye drivers (http://en.ids-imaging.com/download-ueye.html). Also make sure that the IDS daemon (/etc/init.d/ueyeusbdrc) is running.")
 
   include(CheckIncludeFileCXX)
   check_include_file_cxx("uEye.h" FOUND_UEYE_H)
